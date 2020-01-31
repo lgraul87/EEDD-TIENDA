76b1cf8f138386d4a7e02c91623e7676022f361b
Medac <pablo.d.salva@gmail.com>
2020-01-31 14:29:30 +0100

    Soy Pablo y estoy probando una cosa

--- src/EEDDTIENDA.java
+++ src/EEDDTIENDA.java
@@ -3,5 +3,7 @@ public class EEDDTIENDA {
 	public static void main(String[] args) {
 		
 		System.out.println("cambio5");
+		System.out.println("Soy Pablo y estoy cambiando esto");
+		System.out.println("Hola buenas tardes, sigo siendo Pablo");
 	}
 }
